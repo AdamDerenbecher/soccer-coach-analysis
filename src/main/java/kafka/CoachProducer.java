f7b26c7b429c1efca4b6d1d5b33710097740af6d
Adam Derenbecher <adam.derenbecher@gmail.com>
2019-09-11 15:32:13 -0500

    adding logger to producer class

--- src/main/java/kafka/CoachProducer.java
+++ src/main/java/kafka/CoachProducer.java
@@ -1,6 +1,17 @@
 package kafka;
 
-public class CoachProducer {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class CoachProducer {
+
+    private final Logger logger = LoggerFactory.getLogger(CoachProducer.class.getName());
+
     public static void main(String[] args) {
+        new CoachProducer().run();
+    }
+
+    private void run() {
+        logger.info("logger is working!");
     }
 }
